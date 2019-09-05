61015962e53aec4904c8d5c631a0d2f5b7c3074b
oana2.sabau <oana2.sabau@yahoo.com>
2019-09-05 20:29:42 +0300

    create folder "Hello" export with React

--- src/App.test.js
+++ src/App.test.js
@@ -1,4 +1,4 @@
-import React from 'react';
+çimport React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
---
+++ src/Hello.js
@@ -0,0 +1,7 @@
+import React from "react";
+const Hello = () => (
+  <div>
+    <h1>Hello React World</h1>
+  </div>
+);
+export default Hello;
